85a6894fd1dc2b84af314bf8dcb0d1c1c668441f
taehun0933 <taehun0933@naver.com>
2024-10-26 20:58:37 +0900

    github actions 테스트

--- components/Egg.tsx
+++ components/Egg.tsx
@@ -1,7 +1,17 @@
-import styles from "../styles/Egg.module.scss";
+import Image from "next/image";
 
 const Egg: React.FC = () => {
-  return <div className={styles.egg}></div>;
+  return (
+    <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+      <Image
+        src="/images/tmp_eggImage.png"
+        alt="Egg Image"
+        width={400} // 원하는 너비
+        height={0} // height를 0으로 설정하고 layout="intrinsic"으로 높이를 자동 조정
+        layout="intrinsic"
+      />
+    </div>
+  );
 };
 
 export default Egg;
